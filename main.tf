034aac70d70df4aff11ab5caf6cb2d5d2532a1b0
Yutaro Mori <yuta_mori@outlook.jp>
2021-02-20 17:21:21 +0900

    fix main.tf

--- main.tf
+++ main.tf
@@ -13,28 +13,13 @@ terraform {
   }
 }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "DynamoDB State Lock Table"
-  }
-}
 
 resource "aws_instance" "web" {
   instance_type = "t3.small"
   # Amazon Linux2
   ami   = "ami-0992fc94ca0f1415a"
-  count = 1
+  count = 3
   tags = {
-    Name = "EC2 instance terraform"
+    Name = "EC2 instance terraform3"
   }
 }
